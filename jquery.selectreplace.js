390d1abe0f088424d8f05cb73c6d8bc1e4bea4fc
Jason Garber <jason@sixtwothree.org>
2013-01-17 16:22:47 -0500

    Removed $.browser check.

--- jquery.selectreplace.js
+++ jquery.selectreplace.js
@@ -2,36 +2,34 @@
  * @name jQuery selectReplace (https://github.com/jgarber623/jquery-selectReplace)
  * @author Jason Garber
  * @copyright (cc) Jason Garber (http://sixtwothree.org and http://www.viget.com)
- * 
+ *
  * Licensed under the CC-GNU GPL (http://creativecommons.org/licenses/GPL/2.0/)
  */
 
 ;(function($) {
 	$.fn.selectReplace = function( options ) {
-		if ( !( $.browser.msie && $.browser.version < 7 ) ) {
-			return this.each( function() {
-				var opts = $.extend( {}, $.fn.selectReplace.defaults, options ),
-					$this = $( this ),
-					$value = $( '<span class="value" />' ),
-					$wrapper = $( '<span class="select-wrapper" id="' + $this.attr( "id" ) + '-wrapper" />' );
+		return this.each( function() {
+			var opts = $.extend( {}, $.fn.selectReplace.defaults, options ),
+				$this = $( this ),
+				$value = $( '<span class="value" />' ),
+				$wrapper = $( '<span class="select-wrapper" id="' + $this.attr( "id" ) + '-wrapper" />' );
+			
+			$value.html( $.fn.selectReplace.getCurrentValue( $this ) );
+			
+			$this.wrap( $wrapper ).before( $value ).bind({
+				"change.selectReplace": function() {
+					$value.html( $.fn.selectReplace.getCurrentValue( $this ) );
+				},
 				
-				$value.html( $.fn.selectReplace.getCurrentValue( $this ) );
+				"focus.selectReplace": function() {
+					$this.closest( "span.select-wrapper" ).addClass( opts.focusClassName );
+				},
 				
-				$this.wrap( $wrapper ).before( $value ).bind({
-					"change.selectReplace": function() {
-						$value.html( $.fn.selectReplace.getCurrentValue( $this ) );
-					},
-					
-					"focus.selectReplace": function() {
-						$this.closest( "span.select-wrapper" ).addClass( opts.focusClassName );
-					},
-					
-					"blur.selectReplace": function() {
-						$this.closest( "span.select-wrapper" ).removeClass( opts.focusClassName );
-					}
-				});
+				"blur.selectReplace": function() {
+					$this.closest( "span.select-wrapper" ).removeClass( opts.focusClassName );
+				}
 			});
-		}
+		});
 	};
 	
 	$.fn.selectReplace.getCurrentValue = function( select ) {
